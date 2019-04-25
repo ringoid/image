75b26dd116ec69e6963a7c94049b0df89dd72f6f
angryTit <mikhail.tiamine@gmail.com>
2019-04-25 20:06:53 +0300

    fix bug with deletion unresized photo

--- lambda-handle-task/remove_photo.go
+++ lambda-handle-task/remove_photo.go
@@ -23,6 +23,10 @@ func removePhoto(body []byte, lc *lambdacontext.LambdaContext, anlogger *commons
 		return errors.New(errStr)
 	}
 
+	if userPhoto == nil {
+		return nil
+	}
+
 	ok, errStr = commons.DeleteFromS3(userPhoto.Bucket, userPhoto.Key, rTask.UserId, awsS3Client, lc, anlogger)
 	if !ok {
 		return errors.New(errStr)
@@ -59,12 +63,33 @@ func getUserPhoto(userId, photoId, tableName string, lc *lambdacontext.LambdaCon
 		return nil, true, ""
 	}
 
-	res := apimodel.UserPhoto{
-		Bucket: *result.Item[commons.PhotoBucketColumnName].S,
-		Key:    *result.Item[commons.PhotoKeyColumnName].S,
+	if _, ok := result.Item[commons.PhotoDeletedAtColumnName]; ok {
+		anlogger.Warnf(lc, "remove_photo.go : photoId [%s] already deleted for userId [%s]", photoId, userId)
+		return nil, true, ""
 	}
-	anlogger.Debugf(lc, "remove_photo.go : successfully get userPhoto %v for userId [%s], photoId [%s] and table [%s]",
-		res, userId, photoId, tableName)
 
-	return &res, true, ""
+	_, photoDeleted := result.Item[commons.PhotoDeletedAtColumnName]
+	backetAttr, bucketExist := result.Item[commons.PhotoBucketColumnName]
+	keyAttr, keyExist := result.Item[commons.PhotoKeyColumnName]
+
+	if bucketExist && backetAttr != nil && keyExist && keyAttr != nil {
+		res := apimodel.UserPhoto{
+			Bucket: *result.Item[commons.PhotoBucketColumnName].S,
+			Key:    *result.Item[commons.PhotoKeyColumnName].S,
+		}
+		anlogger.Debugf(lc, "remove_photo.go : successfully get userPhoto %v for userId [%s], photoId [%s] and table [%s]",
+			res, userId, photoId, tableName)
+
+		return &res, true, ""
+	}
+
+	if photoDeleted {
+		//it just means that photo was deleted before was resized
+		return nil, true, ""
+	}
+
+	//and this means an error in our soft
+	anlogger.Errorf(lc, "remove_photo.go : try to remove photo with photoId [%s] without bucket or key in DB, userId [%s]",
+		photoId, userId)
+	return nil, false, commons.InternalServerError
 }
